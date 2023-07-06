30b6176f3b471e20424a60f6ad429f8ea15341f2
kvtsang <kvtsang@gmail.com>
2023-07-06 14:37:48 -0700

    [update] re-organize forward and loss functions

--- python/gibuu_ml/models.py
+++ python/gibuu_ml/models.py
@@ -20,36 +20,52 @@ class ParticleTypeModel(pl.LightningModule):
 
         self.save_hyperparameters()
         
-    def training_step(self, batch, batch_idx):
+    def forward(self, batch):
         # Generate src (x_in), tgt (x_out), and masks
         x_in = self.net.embed_input(batch['src_eid'], batch['src_feat'])
-        x_out = self.net.embed_output(batch['tgt_eid'], batch['tgt_feat'])
         src_padding_mask = batch['src_padding_mask']
         
-        # Shift x_out (target entering network), batch['tgt_eid'] (expected result), and relevant masks
-        x_out = x_out[:,:-1]
-        expected_out = batch['tgt_eid'][:,1:]
-        tgt_mask = torch.nn.Transformer.generate_square_subsequent_mask(x_out.size(dim=1), device=x_out.device)
-        tgt_padding_mask = batch['tgt_padding_mask'][:,:-1] # shift padding the same way as x_out
+        # Shift x_out (target entering network), batch['tgt_eid']
+        # (expected result), and relevant masks
+        x_out = self.net.embed_output(
+            batch['tgt_eid'][:,:-1], batch['tgt_feat'][:,:-1]
+        )
+        tgt_mask = torch.nn.Transformer.generate_square_subsequent_mask(
+            x_out.size(dim=1), device=x_out.device,
+        )
+
+        # shift padding the same way as x_out
+        tgt_padding_mask = batch['tgt_padding_mask'][:,:-1]
         
         # Run network
         output = self.net(
-            x_in, x_out, 
+            x_in, x_out,
             tgt_mask=tgt_mask,
-            src_key_padding_mask=src_padding_mask, 
-            tgt_key_padding_mask=tgt_padding_mask, 
+            src_key_padding_mask=src_padding_mask,
+            tgt_key_padding_mask=tgt_padding_mask,
             memory_key_padding_mask=src_padding_mask
         )
-        
-        # Switch output shape from [batch_size, n_particles, n_classes] to [batch_size, n_classes, n_particles]
-        class_out = np.swapaxes(output['class_out'], 2, 1) 
-            
-        # Calculate loss
-        loss = torch.nn.CrossEntropyLoss(ignore_index = 0)
-        loss = loss(class_out, expected_out)
-        
+
+        return output
+
+    @staticmethod
+    def func_loss_part_id(batch, output, **kwargs):
+        # Switch output shape from [batch_size, n_particles, n_classes] 
+        # to [batch_size, n_classes, n_particles]
+
+        loss = torch.nn.functional.cross_entropy(
+            output['class_out'].swapaxes(1,2),
+            batch['tgt_eid'][:,1:],
+            ignore_index=0, 
+            **kwargs
+        )
+
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        output = self(batch)
+        loss = self.func_loss_part_id(batch, output)
         self.log('loss', loss, prog_bar=True)
-        
         return loss
     
     def configure_optimizers(self):
